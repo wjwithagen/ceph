dbafb35bfff15b86ad76409b750cb9d5e12b7089
Willem Jan Withagen <wjw@digiware.nl>
2019-09-08 17:03:14 +0200

    auth: also catch end_of_buffer while parsing text input
    [On FreeBSD]
    in KeyRing::decode() text parsing in only executed if decode throws.
    This results in an ABORT signal in bin/ceph when running vstart.sh
    
    Only buffer::error is caught, but reading config text sometimes
    lets the buffer_length run into absurd numbers.
    Getting out of bounds access in the buffer, generating a trap.
    
    So we need to catch that as well.
    
    However adding buffer::end_of_buffer as exception in another case
    does not seem to work, because it is not matched????
    So catching all exceptions, and continue to perhaps trap in  decode_plaintext()
    
    fixes: https://tracker.ceph.com/issues/41717
    Signed-off-by: Willem Jan Withagen <wjw@digiware.nl>

--- src/auth/KeyRing.cc
+++ src/auth/KeyRing.cc
@@ -203,6 +203,12 @@ void KeyRing::decode(bufferlist::const_iterator& bl) {
   } catch (buffer::error& err) {
     keys.clear();
     decode_plaintext(start_pos);
+  }catch (buffer::end_of_buffer& err) {
+    keys.clear();
+    decode_plaintext(start_pos);
+  } catch (exception& e){
+    keys.clear();
+    decode_plaintext(start_pos);
   }
 }
 
